5fff5a51d5ef787f80f6c5bdc5edad205c8099bf
Rudolf Olah <rudolf.olah.to@gmail.com>
2024-04-01 15:57:09 -0400

    Update Docker image in generate_audio.sh script to use DOCKER_IMAGE env var

--- script/generate_audio.sh
+++ script/generate_audio.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+DOCKER_IMAGE=${DOCKER_IMAGE:-"piper:with-voice"}
 TEXT=$1
 echo "$TEXT" > /tmp/text.txt
-docker run -d -it --entrypoint bash --name piper piper:en-voice
+docker run -d -it --entrypoint bash --name piper $DOCKER_IMAGE
 docker cp /tmp/text.txt piper:/dist/piper/text.txt
 docker exec piper bash -c "cat /dist/piper/text.txt | ./piper --model voice_file.onnx --output_file /dist/piper/output.wav"
 docker cp piper:/dist/piper/output.wav ./output.wav
