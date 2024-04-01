af3a7d5d0d95fd1fc11291fbd0df8bf08da7c150
Rudolf Olah <rudolf.olah.to@gmail.com>
2024-04-01 18:45:28 -0400

    Refactor generate_audio.sh script

--- script/generate_audio.sh
+++ script/generate_audio.sh
@@ -2,9 +2,12 @@
 DOCKER_IMAGE=${DOCKER_IMAGE:-"piper:with-voice"}
 TEXT=$1
 echo "$TEXT" > /tmp/text.txt
-docker run -d -it --entrypoint bash --name piper $DOCKER_IMAGE
+echo "Running the container"
+docker run -d -it --entrypoint sh -w /dist/piper --name piper $DOCKER_IMAGE
 docker cp /tmp/text.txt piper:/dist/piper/text.txt
-docker exec piper bash -c "cat /dist/piper/text.txt | ./piper --model voice_file.onnx --output_file /dist/piper/output.wav"
+echo "Generating the audio"
+docker exec piper sh -c "cat /dist/piper/text.txt | ./piper --model voice_file.onnx --output_file /dist/piper/output.wav"
 docker cp piper:/dist/piper/output.wav ./output.wav
+echo "Stopping and removing the container"
 docker stop piper
 docker rm piper
